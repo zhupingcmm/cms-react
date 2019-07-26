fda640acc119fbe8f2e30a9d5058f1bbe530cdc1
Zhu Ping <pzhu@microfocus.com>
2019-07-26 20:44:40 +0800

    change

--- www/table/Table.js
+++ www/table/Table.js
@@ -3,7 +3,6 @@ import {Route,Link} from 'react-router-dom';
 import StaticPaginationTable from './StaticPaginationTable';
 import DynamicPaginationTable from './DynamicPaginationTable';
 import 'bootstrap/dist/css/bootstrap.css'
-import Test from "./Test";
 
 class Table extends Component{
     constructor(props){
@@ -16,12 +15,10 @@ class Table extends Component{
                     <div className="nav nav-stacked col-lg-1">
                         <li><Link to="/table/static">StaticPaginationTable</Link></li>
                         <li><Link to="/table/dynamic">DynamicPaginationTable</Link></li>
-                        <li><Link to="/table/test">test</Link></li>
                     </div>
                 <div className="col-lg-offset-2">
                     <Route path="/table/static" component={StaticPaginationTable}/>
                     <Route path="/table/dynamic" component={DynamicPaginationTable}/>
-                    <Route path="/table/test" component={Test}/>
                 </div>
             </div>
         );
